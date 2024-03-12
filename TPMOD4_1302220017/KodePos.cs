da93462105a55592841ee6531a2038e56bc62ed1
DanielLieandri21 <dlieandri@gmail.com>
2024-03-12 13:46:39 +0700

    inisialisasi projek

---
+++ TPMOD4_1302220017/KodePos.cs
@@ -0,0 +1,48 @@
+﻿using System;
+namespace tpmod4_1302220017;
+
+using System.Collections.Generic;
+
+public class KodePos
+{
+    private Dictionary<string, string> kodePosDictionary;
+
+    public enum Kelurahan
+    {
+        Antapani_Kidul,
+        Antapani_Kulon,
+        Antapani_Tengah,
+        Antapani_Wetan,
+        Cibaduyut_Kidul,
+        Cibaduyut_Wetan,
+        Cibaduyut,
+        Kebon_lega,
+        Mekarwangi,
+        Situsaeur,
+        Kopo
+    }
+
+    public string GetKodePos(Kelurahan kelurahan)
+    {
+        String[] kodePos =
+            {
+            "40291", // Antapani_Kidul
+            "40291", // Antapani_Kulon
+            "40291", // Antapani_Tengah
+            "40291", // Antapani_Wetan
+            "40239", // Cibaduyut_Kidul
+            "40238", // Cibaduyut_Wetan
+            "40236", // Cibaduyut
+            "40235", // Kebon_lega
+            "40237", // Mekarwangi
+            "40234", // Situsaeur
+            "40233"  // Kopo
+            };
+        int index = (int)kelurahan;
+        if (index >= 0 && index < kodePos.Length)
+        {
+            return kodePos[index];
+        }
+        return "Kode Pos Tidak Ditemukan";
+    }
+}
---
+++ TPMOD4_1302220017/Program.cs
@@ -0,0 +1,34 @@
+﻿using tpmod4_1302220017;
+using static tpmod4_1302220017.KodePos;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Nomor 3 A-C : \n");
+        KodePos kodePos = new KodePos();
+        Console.WriteLine("Kode pos Antapani Kidul\t: " + kodePos.GetKodePos(Kelurahan.Antapani_Kidul));
+        Console.WriteLine("Kode pos Antapani Kulon\t: " + kodePos.GetKodePos(Kelurahan.Antapani_Kulon));
+        Console.WriteLine("Kode pos Antapani Tengah\t: " + kodePos.GetKodePos(Kelurahan.Antapani_Tengah));
+        Console.WriteLine("Kode pos Antapani Wetan\t\t: " + kodePos.GetKodePos(Kelurahan.Antapani_Wetan));
+        Console.WriteLine("Kode pos Cibaduyut Kidul\t: " + kodePos.GetKodePos(Kelurahan.Cibaduyut_Kidul));
+        Console.WriteLine("Kode pos ibaduyut Wetan\t: " + kodePos.GetKodePos(Kelurahan.Cibaduyut_Wetan));
+        Console.WriteLine("Kode pos Cibaduyut\t: " + kodePos.GetKodePos(Kelurahan.Cibaduyut));
+        Console.WriteLine("Kode pos Kebon_Lega\t: " + kodePos.GetKodePos(Kelurahan.Kebon_lega));
+        Console.WriteLine("Kode pos Mekarwangi\t: " + kodePos.GetKodePos(Kelurahan.Mekarwangi));
+        Console.WriteLine("Kode pos Situsaeur\t\t: " + kodePos.GetKodePos(Kelurahan.Situsaeur));
+        Console.WriteLine("Kode pos Kopo\t\t: " + kodePos.GetKodePos(Kelurahan.Kopo));
+
+        // Commit "menambahkan implementasi dengan table-driven"
+
+        Console.WriteLine("\n\n\nNomor 5 A-E : \n");
+
+        DoorMachine door = new DoorMachine();
+        door.DoAction(Action.BukaPintu);
+        door.DoAction(Action.KunciPintu);
+        door.DoAction(Action.KunciPintu); // Kunci lagi, tidak akan terjadi apa-apa karena pintu sudah terkunci
+        door.DoAction(Action.BukaPintu);
+
+        // Commit "menambahkan implementasi dengan state-based construction"
+    }
+}
